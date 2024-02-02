6c62e3ad7355779f312bb471d1673cbaf1eaf77f
Luís Pedro Barros <luispedrandia@aol.com>
2024-02-02 11:51:38 -0300

    bausagd

--- api/index.php
+++ api/index.php
@@ -1,12 +1,11 @@
 <?php
 ?>
 
-<!DOCTYPE html>
 <html lang=pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Luís Pedro - Home</title>
 </head>
 <body>
     
