388f2ca3ee8b8db91d6512f245f5edc89b196911
lpbarros <lpbarros@KETTPC.kettpc>
2025-02-18 00:25:59 -0300

    aaaaa

--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
 git add *
 git commit -m aaaaa -a
 git push
+scp -r . root@187.114.159.233:/drive/0/public_html/old.lpbarros.com
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 git add *
 git commit -m aaaaa -a
 git push
+scp -r . root@187.114.159.233:/drive/0/public_html/old.lpbarros.com
 
